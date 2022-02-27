bc5dc0f5e73bc3064c19438c87c7ad5db9b64161
satishkumarsajjan <55579930+satishkumarsajjan@users.noreply.github.com>
2022-02-28 01:22:56 +0530

    Add files via upload

---
+++ cansum_tabulated.py
@@ -0,0 +1,14 @@
+def cansum_tabulated(targetsum,arr):
+    new_arr=[False for i in range(targetsum+1)]
+    new_arr[0]=True
+    for i in range(targetsum+1):
+        if new_arr[i]==True:
+            for j in arr:
+                if i+j<=targetsum:
+                    new_arr[i+j]=True
+                
+                print(i,new_arr)
+    return new_arr[-1]
+
+
+print(cansum_tabulated(8,[5,6,7]))
