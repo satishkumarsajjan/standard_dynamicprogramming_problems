c67a42204b7b7e90e52bcb8be33cf70e3fd4c694
satishkumarsajjan <55579930+satishkumarsajjan@users.noreply.github.com>
2022-02-28 02:05:07 +0530

    Add files via upload

---
+++ bestsum_tabulated.py
@@ -0,0 +1,16 @@
+def bestsum_tabulated(targetsum,arr):
+    new_arr=[None for i in range(targetsum+1)]
+    new_arr[0]=[]
+    for i in range(targetsum+1):
+        if new_arr[i]!=None:
+            for j in arr:
+                if i+j<=targetsum:
+                    temp=new_arr[i].copy()
+                    temp.append(j)
+                    if new_arr[i+j]==None:
+                        new_arr[i+j]=temp
+                    elif len(temp)<len(new_arr[i+j]):
+                        new_arr[i+j]=temp
+                print(new_arr)
+
+bestsum_tabulated(8,[1,2,4])
